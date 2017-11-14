bc6a169cb52349d064a29ba8eb301aa6e650dfbc
Kei55 <sein.weltall@gmail.com>
2017-11-15 08:52:41 +0900

    test

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var cluster = require('cluster');
+var numCPUs = require('os').cpus().length;
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -9,45 +11,56 @@ var sassMiddleware = require('node-sass-middleware');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
-var app = express();
+if (cluster.isMaster) {
+    for (var i = 0; i < numCPUs; i++) {
+        // Create a worker
+        cluster.fork();
+    }
+} else {
+    // Workers share the TCP connection in this server
+    var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+    // view engine setup
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
-  sourceMap: true
-}));
-app.use(express.static(path.join(__dirname, 'public')));
+    // uncomment after placing your favicon in /public
+    app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')));
+    app.use(logger('dev'));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(cookieParser());
+    app.use(sassMiddleware({
+      src: path.join(__dirname, 'public'),
+      dest: path.join(__dirname, 'public'),
+      indentedSyntax: false, // true = .sass and false = .scss
+      sourceMap: true
+    }));
+    app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+    app.use('/', index);
+    app.use('/users', users);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+    // catch 404 and forward to error handler
+    app.use(function(req, res, next) {
+      var err = new Error('Not Found');
+      err.status = 404;
+      next(err);
+    });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // error handler
+    app.use(function(err, req, res, next) {
+      // set locals, only providing error in development
+      res.locals.message = err.message;
+      res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+      // render the error page
+      res.status(err.status || 500);
+      res.render('error');
+    });
 
-module.exports = app;
+    // All workers use this port
+    // app.listen(8080);
+
+    module.exports = app;
+}
