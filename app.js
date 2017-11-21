bc224239d52837b47c28c896b93fda63a5698251
Kei55 <sein.weltall@gmail.com>
2017-11-21 17:03:14 +0900

    Merge remote-tracking branch 'origin/template' into template
    # Conflicts:
    #	app.js
    #	package.json
    #	public/stylesheets/style.css.map
    #	views/index.pug
    #	views/layout.pug

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var sassMiddleware = require('node-sass-middleware');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -17,6 +20,29 @@ app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(passport.initialize());
+
+passport.use(new LocalStrategy(function(username, password, done){
+    // ここで username と password を確認して結果を返す
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'ユーザーIDが正しくありません。' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'パスワードが正しくありません。' });
+      }
+      return done(null, user);
+    });
+}));
+
+app.post('/',
+  passport.authenticate('local', { successRedirect: '/login',
+                                   failureRedirect: '/',
+                                   failureFlash: true })
+);
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
