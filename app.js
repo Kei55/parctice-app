679e6f860d919c67f2dd9ce66e4d696000e6dcfd
Kei55 <sein.weltall@gmail.com>
2017-11-22 16:42:16 +0900

    bower add

--- app.js
+++ app.js
@@ -7,12 +7,70 @@ var bodyParser = require('body-parser');
 var sassMiddleware = require('node-sass-middleware');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
-var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+//---------ここからpassport-----------
+
+//エラー用フラッシュメッセージのモジュールを設定
+var flash = require('connect-flash');
+app.use(flash());
+
+//passportの初期化
+var passport = require('passport')
+var LocalStrategy = require('passport-local').Strategy;
+app.use(require('express-session')({
+    secret: 'fjaiofjfiafkldsfkadjkafk', //セッションのハッシュ文字列。任意に変更すること。
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+//認証ロジック
+passport.use(
+	new LocalStrategy(
+		{
+			//Viewのアカウント入力フォームのname属性を指定する
+			usernameField: 'user_name',
+			passwordField: 'user_password'
+		},
+		function(username, password, done) {
+			//routerのpassport.authenticate()が呼ばれたらここの処理が走る。
+
+			if(username == 'kama' && password == 'hagehage'){
+				//ログイン成功
+				//今回はここの判定式をハードコーディングにする
+        		return done(null, username);
+			}
+
+			//ログイン失敗
+			//messageはログイン失敗時のフラッシュメッセージ。
+			//各routerの req.flash() で取得できる。
+       		return done(null, false, {message:'ID or Passwordが間違っています。'});
+		}
+	)
+);
+
+
+//認証した際のオブジェクトをシリアライズしてセッションに保存する。
+passport.serializeUser(function(username, done) {
+	console.log('serializeUser');
+	done(null, username);
+});
+
+
+//認証時にシリアライズしてセッションに保存したオブジェクトをデシリアライズする。
+//デシリアライズしたオブジェクトは各routerの req.user で参照できる。
+passport.deserializeUser(function(username, done) {
+	console.log('deserializeUser');
+	done(null, {name:username, msg:'my message'});
+});
+
+//---------ここまでpassport-----------
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,28 +78,6 @@ app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(passport.initialize());
-
-passport.use(new LocalStrategy(function(username, password, done){
-    // ここで username と password を確認して結果を返す
-    User.findOne({ username: username }, function (err, user) {
-      if (err) { return done(err); }
-      if (!user) {
-        return done(null, false, { message: 'ユーザーIDが正しくありません。' });
-      }
-      if (!user.validPassword(password)) {
-        return done(null, false, { message: 'パスワードが正しくありません。' });
-      }
-      return done(null, user);
-    });
-}));
-
-app.post('/',
-  passport.authenticate('local', { successRedirect: '/login',
-                                   failureRedirect: '/',
-                                   failureFlash: true })
-);
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(logger('dev'));
 app.use(bodyParser.json());
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,74 @@
 var express = require('express');
 var router = express.Router();
 
+var passport = require('passport');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //ログイン時のエラーメッセージ表示
+	var errorMsg = '';
+	var error = req.flash().error;
+	if(error){
+		var errorMsg = error[0];
+  }
+
+  res.render('index', { title: 'YKSAMPLE', msg: errorMsg });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('index', { title: 'LOGIN' });
+});
+
+router.get('/signup', function(req, res, next) {
+  res.render('index', { title: 'SIGNUP' });
+});
+
+//ログイン処理
+//badRequestMessage は入力値が空の場合にflashにセットされるメッセージ。
+//つまり、入力値が空かどうかはpassport側でチェックしてくれているので、それ用のバリデーションは不要。
+router.post(
+	'/login',
+	passport.authenticate( 'local', { successRedirect: '/mypage', failureRedirect: '/', failureFlash: true, badRequestMessage: '入力値が空です' }),
+	function(req, res, next) {
+		//認証成功した場合のコールバック
+		//成功時のリダイレクトは successRedirect で指定しているので、ここですることは特にないかもしれない。
+	}
+);
+
+//マイページ（ログイン後のページで認証済みじゃないと見れない）
+router.get('/mypage', function(req, res, next) {
+
+	//log
+	console.log('mypage');
+	console.log(req.user);
+
+	if(req.user){
+		//userが存在する場合は認証済み
+		res.render('index', { title: 'Mypage' });
+	}else{
+		res.redirect('/');
+	}
+});
+
+//設定画面（認証済みじゃないと見れない）
+router.get('/setting', function(req, res, next) {
+
+	//log
+	console.log('mypage');
+	console.log(req.user);
+
+	if(req.user){
+		//userが存在する場合は認証済み
+		res.render('index', { title: 'Setting' });
+	}else{
+		res.redirect('/');
+	}
+});
+
+//ログアウト
+router.get('/logout', function(req, res, next) {
+	req.logout();
+	res.redirect('/');
 });
 
 module.exports = router;
